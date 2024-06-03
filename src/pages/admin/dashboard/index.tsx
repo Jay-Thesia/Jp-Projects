04c1d8f42bd316d95fa6fe0fe9a6304724432d55
Jay Thesia <jayengineer.jp2410@gmail.com>
2024-06-03 07:51:28 +0530

    admin panel's dashboard

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,5 +1,49 @@
+import {
+  useReactTable,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  createColumnHelper,
+} from '@tanstack/react-table';
+
 const AdminDashboard = () => {
-  return <div></div>;
+  const columns = [
+    {
+      accessorKey: 'id',
+      Header: 'ID',
+    },
+    {
+      accessorKey: 'name',
+      Header: 'Name',
+      Cell: ({ row }: any) => row.original.name.toUpperCase(), // Example of custom cell rendering
+    },
+    {
+      accessorKey: 'email',
+      Header: 'Email',
+    },
+  ];
+
+  const data = [
+    { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
+    { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' },
+    // ... more data objects
+  ];
+
+  const tableInstance = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
+  return (
+    <>
+      <div className="">
+        <table></table>
+      </div>
+      ;
+    </>
+  );
 };
 
 export default AdminDashboard;
